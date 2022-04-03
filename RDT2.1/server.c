1509afd32e8fd8e6138f1462b62dc279cb017eca
Daedalus9 <longo.luca94@hotmail.it>
2022-04-03 10:41:41 +0200

    refactor: major refactor for check conditions in rdt_rcv

--- RDT2.1/server.c
+++ RDT2.1/server.c
@@ -14,8 +14,15 @@ int corrupt(char* data) {
     else return 0;
 }
 
-int check_seq(char exp, char flag) {
-    if(exp == flag) {
+int has_seq0(char* rcvpkt) {
+    if(rcvpkt[0]=='0') {
+        return 1;
+    }
+    else return 0;
+}
+
+int has_seq1(char* rcvpkt) {
+    if(rcvpkt[0]=='1') {
         return 1;
     }
     else return 0;
@@ -53,32 +60,42 @@ void deliver_data(char* data) {
 }
 
 void rdt_rcv(char* data, int socket_descriptor, struct sockaddr_in client_address, socklen_t client_address_length) {
-    char flag;
-    char exp_flag='0';
-    while (1) {
+    char exp_flag = '0';
+    while(1) {
         recv(socket_descriptor, data, sizeof(data), 0);
-        flag = data[0];
-        if(corrupt(data)==1) {
+        if(corrupt(data)==0 && has_seq0(data) && exp_flag=='0') {
             deliver_data(data);
+            sleep(2);
+            udt_send("ACK");
+            exp_flag = '1';
+            printf("Now expected flag is %d\n", exp_flag - '0');
+        }
+        else if(corrupt(data)==1) {
+            printf("Corrupt packet received, not delivered: %s\n", data);
             sleep(2);
             udt_send("NACK");
         }
+        else if(corrupt(data)==0 && has_seq0(data) && exp_flag=='1') {
+            printf("Duplicate packet received. Not delivered: %s\n", data);
+            printf("Received flag %d, expected flag %d. Probably caused by corrupt ACK send\n", data[0] - '0', exp_flag - '0');
+            sleep(2);
+            udt_send("ACK");
+        }
+        else if(corrupt(data)==0 && has_seq1(data) && exp_flag=='1') {
+            deliver_data(data);
+            udt_send("ACK");
+            exp_flag = '0';
+            printf("Now expected flag is %d\n", exp_flag - '0');
+        }
+        else if(corrupt(data)==0 && has_seq1(data) && exp_flag=='0') {
+            printf("Duplicate packet received. Not delivered: %s\n", data);
+            printf("Received flag %d, expected flag %d. Probably caused by corrupt ACK send\n", data[0] - '0', exp_flag - '0');
+            sleep(2);
+            udt_send("ACK");
+        }
         else {
-            if(check_seq(exp_flag, flag)==1) {
-                deliver_data(data);
-                sleep(2);
-                udt_send("ACK");
-                if(flag=='0') exp_flag='1';
-                if(flag=='1') exp_flag='0';
-                printf("Now expected flag is %d\n", exp_flag - '0');
-            }
-            else {
-                printf("Received flag %d, expected flag %d. Probably caused by corrupt ACK send\n", flag - '0', exp_flag - '0');
-                if(exp_flag=='0') exp_flag='1';
-                else exp_flag='0';
-                sleep(2);
-                udt_send("ACK");
-            }   
+            printf("Corrupt packet received, not delivered: %s\n", data);
+            udt_send("NACK");
         }
     }
 }
