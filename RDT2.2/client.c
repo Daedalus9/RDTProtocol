0e567e03d73978b1afd9360dfa0780d91905226f
Daedalus9 <longo.luca94@hotmail.it>
2022-04-26 10:31:00 +0200

    feat: add RDT2.2

---
+++ RDT2.2/client.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+int isACK(char* rcvpkt, char* flag) {
+    if(rcvpkt[0]=='A' && flag=="1") {
+        return 1;
+    }
+    else return 0;
+}
+
+int corrupt(char* rcvpkt) {
+    if (rcvpkt[0]!='A') {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
+char* make_pkt(char *flag, char* data, char* checksum) {
+    char* pkt;
+    pkt = malloc(sizeof(data) + sizeof(checksum) + 2);
+    strcpy(pkt, flag);
+    strcat(pkt, data);
+    strcat(pkt, checksum);
+    return pkt;
+}
+
+void rdt_rcv(char* rcvpkt) {
+    int socket_descriptor_server = socket(PF_INET, SOCK_DGRAM, 0);
+    if(socket_descriptor_server == -1) {
+        perror("Unable to initialize socket");
+        exit(1);
+    }
+    struct sockaddr_in server_address;
+    server_address.sin_addr.s_addr=INADDR_ANY;
+    server_address.sin_family=AF_INET;
+    server_address.sin_port=htons(8000);
+
+    socklen_t address_length = sizeof(server_address);
+
+    int bindCode = bind(socket_descriptor_server, (struct sockaddr*) &server_address, address_length);
+    if(bindCode==-1) {
+        perror("Error on bind");
+        exit(1);
+    }
+    
+    ssize_t rcvpkt_length = sizeof(rcvpkt);
+    recv(socket_descriptor_server, rcvpkt, sizeof(rcvpkt), 0);
+    printf("%s\n", rcvpkt);
+    close(socket_descriptor_server);
+}
+
+void udt_send(char* data, int socket_descriptor, struct sockaddr_in server_address, socklen_t server_address_length) {
+    if(rand()%2==0) {
+        char* pkt;
+        pkt = malloc(sizeof(data)+ 1);
+        strcpy(pkt, data);
+        pkt[5] = '1';
+        sendto(socket_descriptor, pkt, sizeof(pkt), 0, (struct sockaddr*) &server_address, server_address_length);
+    }
+    else {
+        sendto(socket_descriptor, data, sizeof(data), 0, (struct sockaddr*) &server_address, server_address_length);
+    }
+    //sendto(socket_descriptor, data, sizeof(data), 0, (struct sockaddr*) &server_address, server_address_length);
+}
+
+void rdt_send(char* data, int socket_descriptor, struct sockaddr_in server_address, socklen_t server_address_length) {
+    char* checksum = "0";
+    char* flag = "0";
+    char* flag_ack="0";
+    char rcvpkt[1024];
+    char* sndpkt;
+    int retransmit = 0;
+    sndpkt = make_pkt(flag_ack, data, checksum);
+    udt_send(sndpkt, socket_descriptor, server_address, server_address_length);
+    sleep(2);
+    char exp_flag='0';
+    while(1) {
+        rdt_rcv(rcvpkt);
+        if(rcvpkt[3]=='0') flag_ack = "0";
+        else flag_ack = "1";
+        if(corrupt(rcvpkt)==1) {
+            printf("Corrupt packet received. Resend last packet\n");
+            udt_send(sndpkt, socket_descriptor, server_address, server_address_length);
+            
+        }
+        else {
+            if(isACK(rcvpkt, flag_ack)==0 && exp_flag=='0') {
+                flag="1";
+                exp_flag='1';
+                sndpkt = make_pkt(flag, data, checksum);
+                udt_send(sndpkt, socket_descriptor, server_address, server_address_length);
+            }
+            else if(isACK(rcvpkt, flag_ack)==1 && exp_flag=='1') {
+                flag="0";
+                exp_flag='0';
+                sndpkt = make_pkt(flag, data, checksum);
+                udt_send(sndpkt, socket_descriptor, server_address, server_address_length);
+            }
+            else if(isACK(rcvpkt, flag_ack)==0 && exp_flag=='1') {
+                printf("ACK is number %d, but expected flag is %d\n", isACK(rcvpkt, flag_ack), exp_flag - '0');
+                udt_send(sndpkt, socket_descriptor, server_address, server_address_length);
+            }
+            else if(isACK(rcvpkt, flag_ack)==1 && exp_flag=='0') {
+                printf("ACK is number %d, but expected flag is %d\n", isACK(rcvpkt, flag_ack), exp_flag - '0');
+                udt_send(sndpkt, socket_descriptor, server_address, server_address_length);
+            }
+        }
+        sleep(2);
+    }
+}
+
+int main() {
+    printf("RDT 2.2 Client\n");
+
+    int socket_descriptor = socket(PF_INET, SOCK_DGRAM, 0);
+    if(socket_descriptor == -1) {
+        perror("Unable to initialize socket");
+        exit(1);
+    }
+
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9000);
+
+    socklen_t server_address_length = sizeof(server_address);
+    int addr_conversion_ret_code = inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr);
+    
+    if(addr_conversion_ret_code<1) {
+        perror("Error on address conversion");
+        exit(1);
+    }
+    
+    char* data = "DATA";
+    rdt_send(data, socket_descriptor, server_address, server_address_length);
+}
---
+++ RDT2.2/server.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+int corrupt(char* data) {
+    if(data[5] - '0' ==1) {
+        return 1;
+    }
+    else return 0;
+}
+
+int has_seq0(char* rcvpkt) {
+    if(rcvpkt[0]=='0') {
+        return 1;
+    }
+    else return 0;
+}
+
+int has_seq1(char* rcvpkt) {
+    if(rcvpkt[0]=='1') {
+        return 1;
+    }
+    else return 0;
+}
+
+void udt_send(char* data, char* flag) {
+    int socket_descriptor_client = socket(PF_INET, SOCK_DGRAM, 0);
+    struct sockaddr_in client_address;
+    client_address.sin_family = AF_INET;
+    client_address.sin_port = htons(8000);
+
+    socklen_t client_address_length = sizeof(client_address);
+    int addr_conversion_ret_code = inet_pton(AF_INET, "127.0.0.1", &client_address.sin_addr);
+    
+    if(addr_conversion_ret_code<1) {
+        perror("Error on address conversion");
+        exit(1);
+    }
+    if(rand()%10==5) { // simulate corrupt response
+        char* cr="corrupt";
+        char* pkt = malloc(strlen(cr)+ 1);
+        strcpy(pkt, cr);
+        sendto(socket_descriptor_client, pkt, sizeof(pkt), 0, (struct sockaddr*) &client_address, client_address_length);
+    }
+    else {
+        char* pkt = malloc(strlen(data)+2);
+        strcpy(pkt, data);
+        strcat(pkt, flag);
+        sendto(socket_descriptor_client, pkt, sizeof(pkt), 0, (struct sockaddr*) &client_address, client_address_length);
+    }
+    close(socket_descriptor_client);
+}
+
+void deliver_data(char* data) {
+    printf("%s\n", data);
+}
+
+void rdt_rcv(char* data, int socket_descriptor, struct sockaddr_in client_address, socklen_t client_address_length) {
+    char exp_flag='0';
+    while(1) {
+        recv(socket_descriptor, data, sizeof(data), 0);
+        if(corrupt(data)==0) {
+            if(has_seq0(data) && exp_flag=='0') {
+                deliver_data(data);
+                sleep(2);
+                udt_send("ACK", "0");
+                exp_flag = '1';
+                printf("Now expected flag is %d\n", exp_flag - '0');
+            }
+            else if(has_seq0(data) && exp_flag=='1') {
+                printf("Duplicate packet received, not delivery: %s\n", data);
+                sleep(2);
+                udt_send("ACK", "0");
+            }
+            else if(has_seq1(data) && exp_flag=='0') {
+                printf("Duplicate packet received, not delivery: %s\n", data);
+                sleep(2);
+                udt_send("ACK", "1");
+            }
+            else if(has_seq1(data) && exp_flag=='1') {
+                deliver_data(data);
+                sleep(2);
+                udt_send("ACK", "1");
+                exp_flag = '0';
+                printf("Now expected flag is %d\n", exp_flag - '0');
+            }
+        }       
+        else {
+            printf("Corrupt packet received, not delivered: %s\n", data);
+            sleep(2);
+            if(exp_flag=='0') udt_send("ACK", "1");
+            else udt_send("ACK", "0");
+        }
+        sleep(2);
+    }
+}
+
+int main() {
+    printf("RDT 2.2 Server\n");
+
+    int socket_descriptor = socket(PF_INET, SOCK_DGRAM, 0);
+    if (socket_descriptor == -1) {
+        perror("Unable to initialize socket");
+        exit(1);
+    }
+
+    struct sockaddr_in address;
+    address.sin_addr.s_addr=INADDR_ANY;
+    address.sin_family=AF_INET;
+    address.sin_port=htons(9000);
+
+    socklen_t address_length = sizeof(address);
+
+    int bindCode = bind(socket_descriptor, (struct sockaddr*) &address, address_length);
+    if(bindCode==-1) {
+        perror("Error on bind");
+        exit(1);
+    }
+
+    char msg_buffer[1024];
+    ssize_t msg_length = sizeof(msg_buffer);
+    rdt_rcv(msg_buffer, socket_descriptor, address, address_length);
+}
